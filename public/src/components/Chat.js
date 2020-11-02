b6de3b36f051ba0f6869f9d764d6972bce5d8c57
Keaton Sentak <k.sentak@outlook.com>
2020-11-01 20:00:04 -0500

    create chat header section

--- public/src/components/Chat.js
+++ public/src/components/Chat.js
@@ -1,9 +1,32 @@
+// Material UI
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+
+// CSS
 import '../css/Chat.css';
 
 const Chat = () => {
 	return (
-		<div className="Chat">
-			<h1>Im a Chat</h1>
+		<div className='chat'>
+			<div className='chat-header'>
+				<Avatar />
+				<div className='chat-headerInfo'>
+					<h3>Room name</h3>
+					<p>Last seen at...</p>
+				</div>
+
+				<div className='chat-headerIcons'>
+					<IconButton>
+						<SearchOutlined />
+					</IconButton>
+					<IconButton>
+						<AttachFile />
+					</IconButton>
+					<IconButton>
+						<MoreVert />
+					</IconButton>
+				</div>
+			</div>
 		</div>
 	);
 };
