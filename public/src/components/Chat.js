6013e0940a21c9a6362204e2abca686b71da9102
Keaton Sentak <k.sentak@outlook.com>
2020-11-02 10:27:52 -0500

    complete chat component

--- public/src/components/Chat.js
+++ public/src/components/Chat.js
@@ -1,6 +1,8 @@
 // Material UI
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 // CSS
 import '../css/Chat.css';
@@ -26,6 +28,29 @@ const Chat = () => {
 						<MoreVert />
 					</IconButton>
 				</div>
+			</div>
+
+			<div className='chat-body'>
+				<p className='chat-message'>
+					<span className='chat-name'>Keaton</span>
+					This is a message
+					<span className='chat-timestamp'>{new Date().toUTCString()}</span>
+				</p>
+
+				<p className='chat-message chat-send'>
+					<span className='chat-name'>Keaton</span>
+					This is a message
+					<span className='chat-timestamp'>{new Date().toUTCString()}</span>
+				</p>
+			</div>
+
+			<div className='chat-footer'>
+				<InsertEmoticonIcon />
+				<form>
+					<input placeholder='Type a message' type='text' />
+					<button type='submit'>Send a message</button>
+				</form>
+				<MicIcon />
 			</div>
 		</div>
 	);
