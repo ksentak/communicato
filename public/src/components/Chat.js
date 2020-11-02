c7741e1cb86cc52bca47115afc0aec82cbce736f
Keaton Sentak <k.sentak@outlook.com>
2020-11-02 16:56:23 -0500

    Update Chat.js

--- public/src/components/Chat.js
+++ public/src/components/Chat.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import axios from '../utils/API';
 // Material UI
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
@@ -8,6 +10,21 @@ import MicIcon from '@material-ui/icons/Mic';
 import '../css/Chat.css';
 
 const Chat = ({ messages }) => {
+	const [input, setInput] = useState('');
+
+	const sendMessage = async (e) => {
+		e.preventDefault();
+
+		await axios.post('/api/v1/messages/new', {
+			name: 'Keaton',
+			message: input,
+			timestamp: '11/02/2020',
+			received: false
+		});
+
+		setInput('');
+	};
+
 	return (
 		<div className='chat'>
 			<div className='chat-header'>
@@ -43,8 +60,15 @@ const Chat = ({ messages }) => {
 			<div className='chat-footer'>
 				<InsertEmoticonIcon />
 				<form>
-					<input placeholder='Type a message' type='text' />
-					<button type='submit'>Send a message</button>
+					<input
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						placeholder='Type a message'
+						type='text'
+					/>
+					<button onClick={sendMessage} type='submit'>
+						Send a message
+					</button>
 				</form>
 				<MicIcon />
 			</div>
